db5f62db5ec25a71fc31215974be55bf09d2afce
Lucas (Lightz) <sky.lucas003@gmail.com>
2024-11-12 19:15:55 -0300

    refactor: refactor generateMagicSquare and upgrade user interaction

--- questao_6.py
+++ questao_6.py
@@ -1,52 +1,52 @@
 def generateMagicSquare(n):
-    # Inicializa o quadrado mágico com zeros
-    square = [[0] * n for _ in range(n)]
+  square = [[0] * n for _ in range(n)]
 
-    # Posiciona o número inicial no meio da primeira linha
-    i, j = 0, n // 2
-    for num in range(1, n * n + 1):
-        square[i][j] = num
-        # Salva a posição atual antes de atualizar
-        new_i, new_j = (i - 1) % n, (j + 1) % n
-        if square[new_i][new_j] != 0:  # Se a posição já está ocupada, desce uma linha
-            i += 1
-        else:
-            i, j = new_i, new_j
+  i, j = 0, n // 2
+  for num in range(1, n * n + 1):
+    square[i][j] = num
+    new_i, new_j = (i - 1) % n, (j + 1) % n
+    if square[new_i][new_j] != 0: 
+      i += 1
+    else:
+      i, j = new_i, new_j
 
-    return square
+  return square
 
 def impressMagicSquare(square):
-    for line in square:
-        print(" ".join(f"{num:2}" for num in line))
+  for line in square:
+    print(" ".join(f"{num:2}" for num in line))
 
-# Solicita a entrada do usuário
-while True:
+def main():
+  while True:
     try:
-        n2 = int(input('\nSeja muito bem vindo ao gerador de quadrado magico escolha uma opcao:\n1- Iniciar\n2- Sair\n'))
+      n2 = int(input('\nSeja muito bem vindo ao gerador de quadrado mágico:\n1 - Iniciar\n2 - Sair\n'))
 
-        if n2 == 2:
+      if n2 == 2:
+        break
+      elif n2 == 1:
+        n = int(input("\nDigite um número ímpar para o tamanho do quadrado mágico (3 ≤ n ≤ 15): "))
+        if n % 2 == 1 and 3 <= n <= 15:
+
+          magicSquare = generateMagicSquare(n)
+          magicConstant = n * (n**2 + 1) // 2
+          print("\nQuadrado Mágico:")
+          impressMagicSquare(magicSquare)
+          print(f"\nConstante Mágica: {magicConstant}")
+
+          repeat = str(input('\nDeseja voltar? [s/n]\n'))
+          if repeat == 'S' or repeat == 's':
+            continue
+          elif repeat == 'N' or repeat == 'n':
             break
-        elif n2 == 1:
-            n = int(input("Digite um número ímpar para o tamanho do quadrado mágico (3 ≤ n ≤ 15): "))
-            if n % 2 == 1 and 3 <= n <= 15:
-
-                magicSquare = generateMagicSquare(n)
-                magicConstant = n * (n**2 + 1) // 2
-                print("\nQuadrado Mágico:")
-                impressMagicSquare(magicSquare)
-                print(f"\nConstante Mágica: {magicConstant}")
-
-                repeat = str(input('Deseja voltar? [S/N]\n'))
-                if repeat == 'S' or repeat == 's':
-                    continue
-                elif repeat == 'N' or repeat == 'n':
-                    break
-                else:
-                    print('Responda com S ou N.')
-                    repeat = str(input('\nDeseja voltar? [S/N]\n'))
-            else:
-                print("Por favor, digite um número ímpar entre 3 e 15.")
+          else:
+            print('\nResponda com S ou N.')
+            repeat = str(input('\nDeseja voltar? [s/n]\n'))
         else:
-            break
+          print("\nPor favor, digite um número ímpar entre 3 e 15.")
+      else:
+          break
     except ValueError:
-        print("Entrada inválida. Por favor, digite um número ímpar entre 3 e 15.")
+      print("\nEntrada inválida. Por favor, digite um número ímpar entre 3 e 15.")
+
+if __name__ == "__main__":
+    main()
